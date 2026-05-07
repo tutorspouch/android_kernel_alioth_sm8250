cfbae25227d5aab5ee74398f57c4e29aa4e78145
tutorspouch <tutors.pouch.0f@proton.me>
2026-05-07 13:05:15 +0200

    Update msg.c

--- ipc/msg.c
+++ ipc/msg.c
@@ -1290,12 +1290,12 @@ static int sysvipc_msg_proc_show(struct seq_file *s, void *it)
 
 void __init msg_init(void)
 {
-	msg_init_ns(&init_ipc_ns);
+    msg_init_ns(&init_ipc_ns);
 
-	if (IS_ENABLED(CONFIG_PROC_STRIPPED))
-		return err;
+    if (IS_ENABLED(CONFIG_PROC_STRIPPED))
+        return;
 
-	ipc_init_proc_interface("sysvipc/msg",
-				"       key      msqid perms      cbytes       qnum lspid lrpid   uid   gid  cuid  cgid      stime      rtime      ctime\n",
-				IPC_MSG_IDS, sysvipc_msg_proc_show);
+    ipc_init_proc_interface("sysvipc/msg",
+                "       key      msqid perms      cbytes       qnum lspid lrpid   uid   gid  cuid  cgid      stime      rtime      ctime\n",
+                IPC_MSG_IDS, sysvipc_msg_proc_show);
 }
